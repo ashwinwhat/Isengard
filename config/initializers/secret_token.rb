10d2c0c36abf22fe600dae4cb7604caca7931429
Ashwin <ashwinve@thoughtworks.com>
2013-10-04 11:55:22 +0530

    Ashwin/Dhivya: Initial commit. Project setup.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Isengard::Application.config.secret_key_base = 'fe38cfb719c80f8d27d5017f38d4c736ea1b32de1fb0db84c21f64f5b194b73cdf7cd946e5ee4ef4dedcf6653ec62043d9cea7216d3bdfb75fd52d29566433e0'
